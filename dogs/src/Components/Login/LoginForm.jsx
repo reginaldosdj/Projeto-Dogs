d4cfc53ca7d35936570b4f5cb155e2f2ed21d90d
Reginaldo <reginaldosdj@yahoo.com>
2025-06-08 03:27:20 -0300

    criação de formulários

---
+++ dogs/src/Components/Login/LoginForm.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+
+const LoginForm = () => {
+  const username = useForm();
+  const password = useForm();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'post',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(),
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
+  }
+  return (
+    <section>
+      <h1>Login</h1>
+      <form action="" onSubmit={handleSubmit}>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        <Button>Entrar</Button>
+      </form>
+      <Link to="/login/cadastre-se">Cadastro</Link>
+    </section>
+  );
+};
+
+export default LoginForm;
