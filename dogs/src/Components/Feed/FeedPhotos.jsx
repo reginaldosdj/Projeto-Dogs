ea6657138c83844e318667ac073724b5cb4d100e
Reginaldo <reginaldosdj@yahoo.com>
2025-06-23 03:08:33 -0300

    scroll infinito

--- dogs/src/Components/Feed/FeedPhotos.jsx
+++ dogs/src/Components/Feed/FeedPhotos.jsx
@@ -5,16 +5,24 @@ import { PHOTO_GET, PHOTOS_GET } from '../../Api';
 import Error from '../Helper/Error';
 import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
-const FeedPhotos = ({ setModalPhoto }) => {
+const FeedPhotos = ({ setModalPhoto, setInfinite, page, user }) => {
   const { data, loading, error, request } = useFetch();
 
   useEffect(() => {
     async function fetchPhotos() {
-      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const total = 6;
+      const { url, options } = PHOTOS_GET({
+        page,
+        total,
+        user,
+      });
       const { response, json } = await request(url, options);
+      if (response && response.ok && json.length < total) {
+        setInfinite(false);
+      }
     }
     fetchPhotos();
-  }, [request]);
+  }, [request, user, page, setInfinite]);
   if (error) return <Error error={error} />;
   if (loading) return <Loading />;
   if (data)
