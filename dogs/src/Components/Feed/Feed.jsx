115a5692d6f965282142ca606dfe55140b24a1c0
Reginaldo <reginaldosdj@yahoo.com>
2025-06-23 03:37:40 -0300

    ajustes

--- dogs/src/Components/Feed/Feed.jsx
+++ dogs/src/Components/Feed/Feed.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import FeedPhotos from './FeedPhotos';
 import FeedModal from './FeedModal';
+import PropTypes from 'prop-types';
 
 const Feed = ({ user }) => {
   const [modalPhoto, setModalPhoto] = useState(null);
@@ -44,6 +45,15 @@ const Feed = ({ user }) => {
       ))}
     </div>
   );
+};
+Feed.defaultProps = {
+  user: 0,
+};
+Feed.prototypes = {
+  user: PropTypes.oneOfType([
+    PropTypes.string.isRequired,
+    PropTypes.number.isRequired,
+  ]),
 };
 
 export default Feed;
